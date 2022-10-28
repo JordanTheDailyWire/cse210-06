e732bd7fc4b21b4304a60857b6752833863a71fe
JordanTheDailyWire <cap21014@byui.edu>
2022-10-27 23:58:17 -0600

    Update.

---
+++ display.py
@@ -0,0 +1,25 @@
+# Import objects
+from objects import Paper, Rock, Scissors
+
+class Display():
+    def __init__(self) -> None:
+        pass
+    def print_guess(self, guess):
+        if isinstance(guess, Scissors):
+            print("0   0")
+            print("  X ")
+            print(" / \\")
+            print("/   \\")
+
+        elif isinstance(guess, Rock):
+            print("  ____")
+            print("/      \\")
+            print("\ ____ /")
+
+        elif isinstance(guess, Paper):
+            print("   _______")
+            print("  /      /,")   
+            print(" /      //")
+            print("/______//")
+        else:
+            print("Unknown Guess Type: You did something wrong")
